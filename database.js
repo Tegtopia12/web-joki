64c77a82c1511a1848a72878931f8bb494fdca49
Tegtopia12 <jayalanangteguh@gmail.com>
2026-08-12 10:56:59 +0700

    Fix SQLite path to /tmp/ for Vercel Serverless

--- database.js
+++ database.js
@@ -1,5 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const dbPath = process.env.VERCEL ? '/tmp/joki.db' : path.resolve(__dirname, 'joki.db');
+
 
 const dbPath = path.resolve(__dirname, 'joki.db');
 const db = new sqlite3.Database(dbPath, (err) => {
