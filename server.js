fb39705beaaaad2416832e71d19543a4e792cba8
Tegtopia12 <jayalanangteguh@gmail.com>
2026-08-12 10:07:40 +0700

    Fix static routing server.js

--- server.js
+++ server.js
@@ -102,6 +102,13 @@ app.get('/api/admin/orders', (req, res) => {
         res.json({ success: true, data: rows });
     });
 });
+// Melayani file statis dari folder public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Menampilkan index.html saat halaman utama dibuka
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
